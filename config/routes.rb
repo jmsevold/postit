99511567c4f26cb71600f2a4fe484b504d4109fd
Jonathan Sevold <Jmsevold@gmail.com>
2014-05-27 12:37:32 -0700

    progressed to lesson 2 lecture 4

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ PostitTemplate::Application.routes.draw do
   resources :posts, except: [:destroy] do
     resources :comments, only: [:create]
   end
+  resources :categories, only: [:new,:create,:show]
 end
